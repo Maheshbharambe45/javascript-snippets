7b0e28d9bf6bac23673304ac685bf97f9e79591f
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2024-09-10 19:16:51 +0530

    Create Favorite_movie_finder.js
    Favorite movie finder simply created using javascript

---
+++ Favorite_movie_finder.js
@@ -0,0 +1,17 @@
+const favoriteMovie = "animal";  
+
+let guess=prompt("Enter my favorite movie");
+
+while((favoriteMovie != guess) && (guess != 'quit'))
+{
+    console.log("Oops! You entered wrong movie");
+    guess=prompt(" Wrong , Please think again my favorite movie");
+}
+console.warn("Yehhh , you guesss right")
+
+
+// options tell someone: 
+// 1.victory
+// 2.abcd2
+// 3.bhramhastra
+// 4.animal
