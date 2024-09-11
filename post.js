4c21faf268dbcf5f2879320d76c4edc8189c6093
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2024-09-11 20:55:14 +0530

    Create post.js
    To store user post data

---
+++ post.js
@@ -0,0 +1,11 @@
+//Created for store for user post data through 'Object Literal' in backend
+
+const post=
+{
+    userName:"@mahesh__bharambe",
+    shares:0,
+    likes:'100+',
+    Comments:['nice','cool','yeah'],
+    tags:['@rohitsharma45','@virat.kohli'],
+    growth:'20+'
+};
