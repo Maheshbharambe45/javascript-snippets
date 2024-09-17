2079856ccab6491bf330e2b9afe62bf9de7bb44c
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2024-09-17 20:39:20 +0530

    Create distructing.js

---
+++ distructing.js
@@ -0,0 +1,10 @@
+let constastant=['mahesh','sagar','kartik','pratik','akash','member'];
+
+let result=[winner,runnerUp,...others]=constastant;
+
+
+//Distructing an array
+// output
+// winner=mahesh;
+// runnerUp=sagar;
+// others=['kartik','pratik','akash','member'];
