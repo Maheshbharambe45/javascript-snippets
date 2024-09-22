f0ac64002f7a62d1f834dbb3e29a3f479c65e02e
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2024-09-22 10:44:28 +0530

    Create memoize.js

---
+++ memoize.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(func) {
+    let cache = {}
+    return function (...args) {
+        let n = JSON.stringify(args)
+        if (n in cache) {
+            return cache[n]
+        } else {
+            let result = func.apply(this , args)
+            cache[n] = result
+            return result
+        }
+    }
+}
+
+
