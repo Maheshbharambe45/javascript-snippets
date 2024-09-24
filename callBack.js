8135d20c53a6f9f33aa0f324fe34aec1f226c710
Mahesh_Bharambe <144551859+Maheshbharambe45@users.noreply.github.com>
2024-09-24 21:41:04 +0530

    Create callBack.js

---
+++ callBack.js
@@ -0,0 +1,9 @@
+function saveData(data){
+    let internetSpeed=Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        console.log("data was stored");
+    }
+    else{
+        console.log("data was not stored");
+    }
+}
